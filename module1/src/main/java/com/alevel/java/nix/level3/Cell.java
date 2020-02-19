ca7a9819fe4d1046faec0f7cb6802183f4ce719f
ThePixel <michiraud@gmail.com>
2020-02-19 02:34:52 +0200

    module 1 level 3

---
+++ module1/src/main/java/com/alevel/java/nix/level3/Cell.java
@@ -0,0 +1,28 @@
+package com.alevel.java.nix.level3;
+
+public class Cell {
+    private boolean live;
+    private int countLiveNeighbours;
+
+    Cell(boolean live){
+        this.live = live;
+    }
+
+    public void setLive(boolean live) {
+        this.live = live;
+    }
+
+    //setCountLiveNeighbours
+    public void setCLN(int countLiveNeighbours) {
+        this.countLiveNeighbours = countLiveNeighbours;
+    }
+
+    //getCountLiveNeighbours
+    public int getCLN() {
+        return countLiveNeighbours;
+    }
+
+    public boolean isLive() {
+        return live;
+    }
+}
---
+++ module1/src/test/java/com/alevel/java/nix/level3/GameOfLifeTest.java
@@ -0,0 +1,25 @@
+package com.alevel.java.nix.level3;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void showCells() {
+        GameOfLife gol = new GameOfLife( 5, 5);
+        gol.showCells();
+        gol.step();
+        System.out.println();
+        gol.showCells();
+    }
+}
