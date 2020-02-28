b13aff8f1c17b224f66baa985736bc7de9b80962
ThePixel <michiraud@gmail.com>
2020-02-28 23:01:08 +0200

    minifix

--- hometasks/src/main/java/com/alevel/nix/java/hometask/lesson10/MedianOfTwoSortedArrays.java
+++ hometasks/src/main/java/com/alevel/nix/java/hometask/lesson10/MedianOfTwoSortedArrays.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 public class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int nums[] = new int[nums1.length+nums2.length];
+        int length = nums.length;
+        int length1 = nums1.length;
+        int length2 = nums2.length;
 
-        System.arraycopy(nums1, 0, nums, 0, nums1.length);
-        System.arraycopy(nums2, 0, nums, nums1.length, nums2.length);
+        System.arraycopy(nums1, 0, nums, 0, length1);
+        System.arraycopy(nums2, 0, nums, length1, length2);
 
         Arrays.parallelSort(nums);
 
-        if (nums.length%2 != 0){
-            return nums[(nums.length-1)/2];
+        if (length%2 != 0){
+            return nums[(length-1)/2];
         }else{
-            return (double) (nums[nums.length/2] + nums[nums.length/2-1])/2;
+            return (nums[length/2] + nums[length/2-1]) / 2.0;
         }
     }
 
--- hometasks/src/test/java/com/alevel/nix/java/hometask/lesson10/MedianOfTwoSortedArraysTest.java
+++ hometasks/src/test/java/com/alevel/nix/java/hometask/lesson10/MedianOfTwoSortedArraysTest.java
@@ -20,14 +20,12 @@ class MedianOfTwoSortedArraysTest {
     void findMedianSortedArrays() {
         test(2,new int[]{1,3},new int[]{2});
         test(2.5, new int[]{1,2}, new int[]{3,4});
-//        test(9,new int[]{3,9,1,88}, new int[]{2,77,59});
-//        test(34,new int[]{3,9,1,88}, new int[]{2,77,59,99});
+        test(9,new int[]{3,9,1,88}, new int[]{2,77,59});
+        test(34,new int[]{3,9,1,88}, new int[]{2,77,59,99});
     }
 
-    void test(double expRes, int[] nums1, int[] nums2){{
+    void test(double expRes, int[] nums1, int[] nums2){
         MedianOfTwoSortedArrays motsa = new MedianOfTwoSortedArrays();
         assertEquals(expRes, motsa.findMedianSortedArrays(nums1, nums2));
-    }
-
     }
 }
