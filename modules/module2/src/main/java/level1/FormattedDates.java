99932c4a230bac969b5be7b1812c22959de4caf1
ThePixel <michiraud@gmail.com>
2020-04-06 21:16:22 +0300

    module2 task1 fix

--- modules/module2/src/main/java/level1/FormattedDates.java
+++ modules/module2/src/main/java/level1/FormattedDates.java
@@ -1,38 +1,61 @@
 package level1;
 
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class FormattedDates {
 
     public List<String> getListFormattedDates(Collection<String> dates){
         Objects.requireNonNull(dates);
-
         List<String> list = new ArrayList<>();
 
         for (String date : dates) {
-            try {
-                LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
-                list.add(date);
-            } catch (Exception ignored){}
+            Optional<LocalDate> validDate = getValidDate(date);
+
+            if(validDate.isPresent()){
+                String format = validDate.get().format(DateTimeFormatter.BASIC_ISO_DATE);
+                list.add(format);
+            }
+
         }
 
         return list;
     }
 
+    private Optional<LocalDate> getValidDate(String date){
+        DateTimeFormatter[] formatters = new DateTimeFormatter[]{
+                DateTimeFormatter.ofPattern("yyyyMMdd"),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd"),
+                DateTimeFormatter.ofPattern("dd/MM/yyyy"),
+                DateTimeFormatter.ofPattern("MM-dd-yyyy")
+        };
+
+        for (var formatter : formatters) {
+            try {
+                LocalDate parse = LocalDate.parse(date, formatter);
+                return Optional.ofNullable(parse);
+            } catch (Exception ignored){
+
+            }
+        }
+
+        return Optional.empty();
+    }
+
+
     public static void main(String[] args) {
         FormattedDates f = new FormattedDates();
         List<String> list = new ArrayList<>();
         list.add("2020/04/05");
+        list.add("");
+        list.add("1ffs32");
         list.add("05/04/2020");
         list.add("04-05-2020");
         list.add("20200405");
 
         f.getListFormattedDates(list).forEach(System.out::println);
+
+
     }
 }
