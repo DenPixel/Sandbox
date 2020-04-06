fb0e824075f80c8e427ead65a2c1cac13d2719ea
ThePixel <michiraud@gmail.com>
2020-04-06 21:42:59 +0300

    module2 task1 fixed

--- modules/module2/src/main/java/level1/FormattedDates.java
+++ modules/module2/src/main/java/level1/FormattedDates.java
@@ -5,36 +5,33 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class FormattedDates {
+    private final static DateTimeFormatter[] FORMATTERS = new DateTimeFormatter[]{
+            DateTimeFormatter.ofPattern("yyyyMMdd"),
+            DateTimeFormatter.ofPattern("yyyy/MM/dd"),
+            DateTimeFormatter.ofPattern("dd/MM/yyyy"),
+            DateTimeFormatter.ofPattern("MM-dd-yyyy")
+    };
+
 
     public List<String> getListFormattedDates(Collection<String> dates){
         Objects.requireNonNull(dates);
         List<String> list = new ArrayList<>();
 
         for (String date : dates) {
-            Optional<LocalDate> validDate = getValidDate(date);
 
-            if(validDate.isPresent()){
-                String format = validDate.get().format(DateTimeFormatter.BASIC_ISO_DATE);
-                list.add(format);
-            }
+            getValidDate(date).ifPresent(list::add);
 
         }
 
         return list;
     }
 
-    private Optional<LocalDate> getValidDate(String date){
-        DateTimeFormatter[] formatters = new DateTimeFormatter[]{
-                DateTimeFormatter.ofPattern("yyyyMMdd"),
-                DateTimeFormatter.ofPattern("yyyy/MM/dd"),
-                DateTimeFormatter.ofPattern("dd/MM/yyyy"),
-                DateTimeFormatter.ofPattern("MM-dd-yyyy")
-        };
+    private Optional<String> getValidDate(String date){
 
-        for (var formatter : formatters) {
+        for (var formatter : FORMATTERS) {
             try {
                 LocalDate parse = LocalDate.parse(date, formatter);
-                return Optional.ofNullable(parse);
+                return Optional.of(parse.format(DateTimeFormatter.BASIC_ISO_DATE));
             } catch (Exception ignored){
 
             }
@@ -55,7 +52,6 @@ public class FormattedDates {
         list.add("20200405");
 
         f.getListFormattedDates(list).forEach(System.out::println);
-
 
     }
 }
