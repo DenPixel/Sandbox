560813bedf302ee0a86c35222c94637ac45abe9a
ThePixel <michiraud@gmail.com>
2020-05-21 16:18:42 +0300

    multithreating

---
+++ hometasks/src/main/java/com/alevel/nix/java/hometask/multithreating/ThreadIOConsoleRunner.java
@@ -0,0 +1,21 @@
+package com.alevel.nix.java.hometask.multithreating;
+
+public class ThreadIOConsoleRunner {
+    public static void main(String[] args) {
+        String output = "output.txt";
+
+        if (args.length > 0){
+            output = args[0];
+        }
+
+        ThreadIOConsole tioc = new ThreadIOConsole(output);
+
+        Thread thread = new Thread(tioc);
+        thread.start();
+
+        while (thread.isAlive()) {
+            tioc.consoleInput();
+        }
+
+    }
+}
